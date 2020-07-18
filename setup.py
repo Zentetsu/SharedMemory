9a632a0732130e2f6dd0dd5745f01d957873a9a9
Zentetsu <10742751+Zentetsu@users.noreply.github.com>
2020-07-19 00:04:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Author: Zentetsu
 
 ----
 
-Last Modified: Sat Jul 18 2020
+Last Modified: Sun Jul 19 2020
 Modified By: Zentetsu
 
 ----
@@ -39,7 +39,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='SharedMemory',
-    version='0.2.0-dev0',
+    version='0.2.1-dev0',
     author='Zentetsu',
     packages=find_packages(exclude=['tests*']),
     license='GPLv3',

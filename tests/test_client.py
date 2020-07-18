c2a6cc39337d7ae3f9b8ad96c064aacbb6539d34
Zentetsu <10742751+Zentetsu@users.noreply.github.com>
2020-07-18 16:34:51 +0200

    Adding more tests

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,7 +5,7 @@ Author: Zentetsu
 
 ----
 
-Last Modified: Thu Jul 16 2020
+Last Modified: Sat Jul 18 2020
 Modified By: Zentetsu
 
 ----
@@ -31,50 +31,51 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----
 
 HISTORY:
+2020-07-18	Zen	Adding new tests
 2020-07-03	Zen	Adding new tests
 2020-07-02	Zen	Adding test with json file
 2020-07-01	Zen	Creating file
 '''
 
-from context import Client
-# from SharedMemory.Client import Client
+# from context import Client
+from SharedMemory.Client import Client
 
 def test_str():
-    c = Client("test", "azerty")
+    c = Client("test1", "azerty")
     print(c)
     assert type(c.getValue()) is str
     c.stop()
 
 def test_int():
-    c = Client("test", 1)
+    c = Client("test2", 1)
     print(c)
     assert type(c.getValue()) is int
     assert c.getValue() == 1
     c.stop()
 
 def test_float():
-    c = Client("test", 1.2)
+    c = Client("test3", 1.2)
     print(c)
     assert type(c.getValue()) is float
     assert c.getValue() == 1.2
     c.stop()
 
 def test_bool():
-    c = Client("test", True)
+    c = Client("test4", True)
     print(c)
     assert type(c.getValue()) is bool
     assert c.getValue()
     c.stop()
 
 def test_dict1():
-    c = Client("test", {'a': 1})
+    c = Client("test5", {'a': 1})
     print(c)
     assert type(c.getValue()) is dict
     assert c.getValue()['a'] == 1
     c.stop()
 
 def test_list1():
-    c = Client("test", [0, 1])
+    c = Client("test6", [0, 1])
     print(c)
     assert type(c.getValue()) is list
     assert c.getValue()[0] == 0
@@ -88,7 +89,8 @@ def test_list1():
 #     pass
 
 def test_list2():
-    c = Client("test", [int, bool, str])
+    c = Client("test7", [int, bool, str])
+    print(c)
     assert type(c.getValue()) is list
     assert type(c.getValue()[0]) is int
     assert type(c.getValue()[1]) is bool
@@ -96,7 +98,8 @@ def test_list2():
     c.stop()
 
 def test_list3():
-    c = Client("test", [int, 10.2, str])
+    c = Client("test8", [int, 10.2, str])
+    print(c)
     assert type(c.getValue()) is list
     assert type(c.getValue()[0]) is int
     assert type(c.getValue()[1]) is float
@@ -104,23 +107,43 @@ def test_list3():
     c.stop()
 
 def test_file():
-    c = Client("test", path="./tests/test.json")
+    c = Client("test9", path="./tests/test.json")
+    print(c)
     assert type(c.getValue()) is dict
     c.stop()
 
 def test_new_value():
-    c = Client("test", 1)
+    c = Client("test10", 1)
     c.updateValue(12)
+    print(c)
     assert c.getValue() == 12
     c.stop()
 
+def test_stop():
+    c = Client("test11", 1)
+    print(c)
+    c.stop()
+    print(c)
+
 test_str()
+print("-"*10)
 test_int()
+print("-"*10)
 test_float()
+print("-"*10)
 test_bool()
+print("-"*10)
 test_dict1()
+print("-"*10)
 test_list1()
+print("-"*10)
 test_list2()
+print("-"*10)
 test_list3()
+print("-"*10)
 test_file()
+print("-"*10)
 test_new_value()
+print("-"*10)
+test_stop()
+print("-"*10)
--- tests/test_server.py
+++ tests/test_server.py
@@ -5,7 +5,7 @@ Author: Zentetsu
 
 ----
 
-Last Modified: Sat Jul 04 2020
+Last Modified: Sat Jul 18 2020
 Modified By: Zentetsu
 
 ----
@@ -31,21 +31,83 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----
 
 HISTORY:
+2020-07-18	Zen	Adding some tests
 2020-07-03	Zen	Updating test file
 2020-07-01	Zen	Creating file
 '''
 
-
+# from context import Client, Server
 from SharedMemory.Server import Server
 
-while 1:
+def test_connection():
+    c = Client("test1", "azerty")
     try:
-        s = Server("test")
-        print(s)
-        s.stop()
+        s = Server("test1")
+        assert True
     except:
-        break
+        assert False
 
-# s = Server("test")
-# print(s)
-# s.stop()
+    print(c)
+    print(s)
+    c.stop()
+    s.stop()
+
+def test_value():
+    c = Client("test2", "azerty")
+    s = Server("test2")
+    print(c)
+    print(s)
+    assert s.getValue() == "azerty"
+    c.stop()
+    s.stop()
+
+def test_edit_value():
+    c = Client("test3", "azerty")
+    s = Server("test3")
+    print(c)
+    print(s)
+    s.updateValue("toto")
+    assert c.getValue() == "toto"
+    print(c)
+    print(s)
+    c.stop()
+    s.stop()
+
+def test_client_stopped():
+    c = Client("test4", "azerty")
+    s = Server("test4")
+    print(c)
+    print(s)
+    c.stop()
+    s.updateValue("toto")
+    assert s.getValue() == "azerty"
+    print(c)
+    print(s)
+    s.stop()
+
+def test_server_stopped():
+    c = Client("test5", "azerty")
+    s = Server("test5")
+    print(c)
+    print(s)
+    s.stop()
+    c.updateValue("toto")
+    assert c.getValue() == "toto"
+    print(s)
+    print(c)
+    s.connect()
+    print(c)
+    print(s)
+    c.stop()
+
+print("-"*10)
+test_connection()
+print("-"*10)
+test_value()
+print("-"*10)
+test_edit_value()
+print("-"*10)
+test_client_stopped()
+print("-"*10)
+test_server_stopped()
+print("-"*10)

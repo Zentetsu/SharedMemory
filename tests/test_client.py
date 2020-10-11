d5a537ddba2548156ded229f579c00f4f93666f8
Zentetsu <10742751+Zentetsu@users.noreply.github.com>
2020-10-11 16:01:11 +0200

    Updating test

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,7 +5,7 @@ Author: Zentetsu
 
 ----
 
-Last Modified: Thu Sep 17 2020
+Last Modified: Sun Oct 11 2020
 Modified By: Zentetsu
 
 ----
@@ -31,6 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ----
 
 HISTORY:
+2020-10-11	Zen	Updating test
 2020-09-17	Zen	Adding test for overloaded methods
 2020-07-18	Zen	Adding new tests
 2020-07-03	Zen	Adding new tests
@@ -40,157 +41,227 @@ HISTORY:
 
 # from context import Client
 from SharedMemory.Client import Client
+import contextlib
 
 def test_str():
-    c = Client("test1", "azerty")
-    print(c)
-    assert type(c.getValue()) is str
-    c.stop()
+    print("Create Client instance containing a \"string\":", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test1", "azerty")
+            assert type(c.getValue()) is str
+            assert type(c[0]) is str
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_int():
-    c = Client("test2", 1)
-    print(c)
-    assert type(c.getValue()) is int
-    assert c.getValue() == 1
-    c.stop()
+    print("Create Client instance containing an \"int\":", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test2", 1)
+            assert type(c.getValue()) is int
+            assert type(c[0]) is int
+            assert c.getValue() == 1
+            assert c[0] == 1
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_float():
-    c = Client("test3", 1.2)
-    print(c)
-    assert type(c.getValue()) is float
-    assert c.getValue() == 1.2
-    c.stop()
+    print("Create Client instance containing a \"float\":", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test3", 1.2)
+            assert type(c.getValue()) is float
+            assert type(c[0]) is float
+            assert c.getValue() == 1.2
+            assert c[0] == 1.2
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_bool():
-    c = Client("test4", True)
-    print(c)
-    assert type(c.getValue()) is bool
-    assert c.getValue()
-    c.stop()
+    print("Create Client instance containing a \"boolean\":", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test4", True)
+            assert type(c.getValue()) is bool
+            assert type(c[0]) is bool
+            assert c.getValue()
+            assert c[0]
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_dict1():
-    c = Client("test5", {'a': 1})
-    print(c)
-    assert type(c.getValue()) is dict
-    assert c.getValue()['a'] == 1
-    c.stop()
+    print("Create Client instance containing a \"dict\":", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test5", {'a': 1})
+            assert type(c.getValue()) is dict
+            assert c.type is dict
+            assert c.getValue()['a'] == 1
+            assert c['a'] == 1
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_list1():
-    c = Client("test6", [0, 1])
-    print(c)
-    assert type(c.getValue()) is list
-    assert c.getValue()[0] == 0
-    assert c.getValue()[1] == 1
-    c.stop()
-
-# def test_1():
-#     # c = Client("test", (0, 1))
-#     # assert type(c.getValue()) is int
-#     # c.stop()
-#     pass
+    print("Create Client instance containing a \"list\":", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test6", [0, 1])
+            assert type(c.getValue()) is list
+            assert c.type is list
+            assert c.getValue()[0] == 0
+            assert c.getValue()[1] == 1
+            assert c[0] == 0
+            assert c[1] == 1
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_list2():
-    c = Client("test7", [int, bool, str])
-    print(c)
-    assert type(c.getValue()) is list
-    assert type(c.getValue()[0]) is int
-    assert type(c.getValue()[1]) is bool
-    assert type(c.getValue()[2]) is str
-    c.stop()
+    print("Create Client instance containing a \"[int, bool, str]\":", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test7", [int, bool, str])
+            assert type(c.getValue()) is list
+            assert c.type is list
+            assert type(c.getValue()[0]) is int
+            assert type(c.getValue()[1]) is bool
+            assert type(c.getValue()[2]) is str
+            assert type(c[0]) is int
+            assert type(c[1]) is bool
+            assert type(c[2]) is str
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_list3():
-    c = Client("test8", [int, 10.2, str])
-    print(c)
-    assert type(c.getValue()) is list
-    assert type(c.getValue()[0]) is int
-    assert type(c.getValue()[1]) is float
-    assert type(c.getValue()[2]) is str
-    c.stop()
+    print("Create Client instance containing a \"[int, 10.2, str]\":", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test8", [int, 10.2, str])
+            assert type(c.getValue()) is list
+            assert c.type is list
+            assert type(c.getValue()[0]) is int
+            assert type(c.getValue()[1]) is float
+            assert type(c.getValue()[2]) is str
+            assert type(c[0]) is int
+            assert type(c[1]) is float
+            assert type(c[2]) is str
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_file():
-    c = Client("test9", path="./tests/test.json")
-    print(c)
-    assert type(c.getValue()) is dict
-    c.stop()
+    print("Create Client instance from JSON file:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test9", path="./tests/test.json")
+            assert type(c.getValue()) is dict
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
-def test_new_value():
-    c = Client("test10", 1)
-    c.updateValue(12)
-    print(c)
-    assert c.getValue() == 12
-    c.stop()
+def test_newValue():
+    print("Update Client value:", end=" ")
+    try:
+        with contextlib.redirect_stdout(None):
+            c = Client("test10", 1)
+            c.updateValue(12)
+            assert c.getValue() == 12
+            assert c[0] == 12
+            c.stop()
+        print("SUCCESSED")
+    except:
+        print("FAILED")
+        assert False
 
 def test_stop():
-    c = Client("test11", 1)
-    print(c)
-    c.stop()
+    print("Stop Client:", end=" ")
     try:
-        print(c)
+        with contextlib.redirect_stdout(None):
+            c = Client("test11", 1)
+            c.stop()
         assert True
+        print("SUCCESSED")
     except:
+        print("FAILED")
         assert False
 
 def test_call():
-    c = Client("test12", 1)
-    print(c)
-    c.stop()
+    print("Stop Client 2:", end=" ")
     try:
-        c.stop()
-        print(c)
+        with contextlib.redirect_stdout(None):
+            c = Client("test12", 1)
+            c.stop()
+            c.stop()
         assert True
+        print("SUCCESSED")
     except:
+        print("FAILED")
         assert False
 
 def test_call2():
-    c = Client("test13", 1)
-    print(c)
+    print("Stop Client 3:", end=" ")
     try:
-        c.start()
-        print(c)
+        with contextlib.redirect_stdout(None):
+            c = Client("test13", 1)
+            c.start()
+            c.stop()
         assert True
+        print("SUCCESSED")
     except:
+        print("FAILED")
         assert False
-    c.stop()
 
 def test_valueAccess():
-    c = Client("test13", {'0':0, '1':1, '2':2, '3':3})
-    print(c)
+    print("Deleting value from overloaded method", end=" ")
     try:
-        c.start()
-        print(c)
-        del c['0']
-        print(c)
-
+        with contextlib.redirect_stdout(None):
+            c = Client("test13", {'0':0, '1':1, '2':2, '3':3})
+            c.start()
+            del c['0']
+            c.stop()
         assert True
+        print("SUCCESSED")
     except:
+        print("FAILED")
         assert False
-    c.stop()
 
+print("-"*10)
 test_str()
-print("-"*10)
 test_int()
-print("-"*10)
 test_float()
-print("-"*10)
 test_bool()
-print("-"*10)
 test_dict1()
-print("-"*10)
 test_list1()
-print("-"*10)
 test_list2()
-print("-"*10)
 test_list3()
-print("-"*10)
 test_file()
-print("-"*10)
-test_new_value()
-print("-"*10)
+test_newValue()
 test_stop()
-print("-"*10)
 test_call()
-print("-"*10)
 test_call2()
-print("-"*10)
 test_valueAccess()
 print("-"*10)
